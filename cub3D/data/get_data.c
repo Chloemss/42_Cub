9d2feef01a50d6103b3d1ee4763d573a8b8b8e17
chloe <clomas57130@gmail.com>
2023-05-04 17:58:50 +0200

    fenetre fixe

--- cub3D/data/get_data.c
+++ cub3D/data/get_data.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_data.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ceatgie <ceatgie@student.42.fr>            +#+  +:+       +#+        */
+/*   By: massavie <massavie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 13:21:58 by ceatgie           #+#    #+#             */
-/*   Updated: 2023/05/02 13:22:08 by ceatgie          ###   ########.fr       */
+/*   Updated: 2023/05/04 17:58:04 by massavie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@ void	x_and_y_map(t_cub *cub)
 
 	x = 0;
 	cub->map_x = ft_strlen(cub->map[x]);
-	cub->xscreen = cub->map_x * 64;
+	cub->xscreen = 720;
 	while (cub->map[x] != NULL)
 		x++;
 	cub->map_y = x;
-	cub->yscreen = cub->map_y * 64;
+	cub->yscreen = 1024;
 }
